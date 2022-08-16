1c7c43a6ab6c5138f4999b38438867b47ae5f39d
Michel Holzky <doc.snuggles@web.de>
2022-08-17 00:19:11 +0200

    exercise 5 beginning

--- exercises/ex5/exercise5.py
+++ exercises/ex5/exercise5.py
@@ -1,14 +1,54 @@
-# print("\n---TASK 7--- \n")
-# print('* ',2 * '* ', 3 * '* ', 4 * '* ', sep='\n')
-# n=5
-# for i in range(n, 0, -1):
-#     for j in range(i):
-#         print('* ', end="")
-#     print('')
-# 
+# Task 1
+print("--- Task 1 ---")
 
-# 0, 1, 1, 2, 3, 5, 8
-x, y = 0, 1
-while y < 50:
-    print(y)
-    x, y = y, x + y
+def user_promt():
+    """
+        ask user to input three values
+    """
+
+    first = int(input("First number, please: "))
+    second = int(input("Second number, please: "))
+    third = int(input("Third number, please: "))
+
+    return first, second, third
+
+
+def calc(num1, num2, num3):
+    """
+        if all numbers are equal, sum all and triple the result
+        otherwise just calculate the sum of all numbers
+    """
+
+    result = num1 + num2 + num3
+    result = result * 3 if num1 == num2 == num3 else result # overwrite result
+
+    return result
+
+
+# user_nums = user_promt()
+# print(calc(*user_nums))
+
+
+# Task 2
+print("--- Task 2 ---")
+
+def user_input():
+    first = int(input("First number: "))
+    second = int(input("Second number: "))
+    
+    return first, second
+
+
+def difference(num1, num2):
+    result = num1 - num2
+
+    if result > 0:
+        result = result * 2
+    else:
+        result = abs(result)
+
+    return "The result is " + str(result)
+
+
+two_numbers = user_input()
+print(difference(*two_numbers))
