6db33722574030e83f23ad9c605add851b700417
Michel Holzky <doc.snuggles@web.de>
2022-08-17 15:42:56 +0200

    ex5 Task2

--- exercises/ex5/exercise5.py
+++ exercises/ex5/exercise5.py
@@ -50,5 +50,85 @@ def difference(num1, num2):
     return "The result is " + str(result)
 
 
-two_numbers = user_input()
-print(difference(*two_numbers))
+# two_numbers = user_input()
+# print(difference(*two_numbers))
+
+
+
+# Task 3
+print("--- Task 3 ---")
+
+
+def odd_or_even(num):
+    result = "even" if num % 2 == 0 else "odd"
+    txt = f"{num} is an {result} number"
+    return txt
+    
+
+print(odd_or_even(10))
+
+
+
+# Task 4
+print("--- Task 4 ---")
+from distutils.command.install_egg_info import to_filename
+from math import pi 
+
+
+def circle_area(radius):
+
+    area = round(pi * radius ** 2, 2)
+    return area
+
+
+# print(circle_area(3.45))
+
+
+
+# Task 5
+print("--- Task 5 ---")
+# its an useless complicated solution
+# but it was a good exercise for recursion 
+to_guess = 8
+
+
+def user_guess():
+    guess = int(input("Guess from 1 to 9: "))
+    return comparison(guess)
+
+
+def comparison(guess):
+    while guess != to_guess:
+        print("Sorry. Wrong number. Try again.")
+        return user_guess()
+    
+    # is executed once, if guess is correct
+    return "Well guessed: " + str(guess) 
+
+
+# print(user_guess())
+
+
+
+# Task 6
+print("--- Task 6 ---")
+
+
+def celsius_fahrenheit(temp, scale):
+    if scale == "C":
+        result = (temp * 1.8) + 32
+    elif scale == "F":
+        result = (temp - 32) / 1.8
+
+    return result
+
+
+def temp_input():
+    temperatur = int(input("What is the temperature to convert: "))
+    scale = input("What is the scale Shortcut (F or C): ")
+
+    return temperatur, scale
+
+
+user_temps = temp_input()
+print(celsius_fahrenheit(*user_temps))
