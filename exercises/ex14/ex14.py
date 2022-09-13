88fa548ec864b0e750243d3daaea8eb9effbc697
Michel Holzky <doc.snuggles@web.de>
2022-09-13 22:41:12 +0200

    ex14–ex16

---
+++ exercises/ex14/ex14.py
@@ -0,0 +1,15 @@
+# Create a function that takes a string input as a number and replaces leading and trailing zeros.
+# 
+# ## Input/Output:
+# "0023.07623070"   -->   "23.0762307"  
+# "hello world"     -->   "hello world"  
+# "01230"           -->   "1230"  
+
+def deleteTrailing(txt):
+    print(txt)
+    
+
+txt1 = "0023.07623070"
+txt2 = "hello world"
+txt3 = "01230"
+print(deleteTrailing(txt))
