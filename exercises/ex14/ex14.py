1c44edcf7f9ec71779a2023953f86e74ef2ed12b
Michel Holzky <doc.snuggles@web.de>
2022-09-16 16:51:36 +0200

    merge

--- exercises/ex14/ex14.py
+++ exercises/ex14/ex14.py
@@ -2,11 +2,11 @@ import re
 
 
 # Create a function that takes a string input as a number and replaces leading and trailing zeros.
-# 
+#
 # ## Input/Output:
-# "0023.07623070"   -->   "23.0762307"  
-# "hello world"     -->   "hello world"  
-# "01230"           -->   "1230"  
+# "0023.07623070"   -->   "23.0762307"
+# "hello world"     -->   "hello world"
+# "01230"           -->   "1230"
 
 def deleteTrailing(txt):
     print(txt)
@@ -14,10 +14,10 @@ def deleteTrailing(txt):
     in_groups = re.finditer(pattern, txt)
     for i in in_groups:
         print(i)
-    
 
 
 txt1 = "0023.0762307000"
 txt2 = "hello world"
 txt3 = "01230"
+
 print(deleteTrailing(txt))
